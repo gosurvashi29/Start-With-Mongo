dba748dbbc5a64cb7fa39ef9cf5f01f640f9e7bb
gosurvashi29 <urvashigoswami350@gmail.com>
2025-04-05 16:10:26 +0530

    mongo new

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
     'mongodb+srv://admin:admin1234@cluster0.0lljc5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
